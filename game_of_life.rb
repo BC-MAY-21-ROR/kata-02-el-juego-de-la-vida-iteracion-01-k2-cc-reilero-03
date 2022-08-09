d72fe2e9e047771dc5c0aaae4301c25c28454ad2
Jorge Vargas <eduardo.borisq@gmail.com>
2022-08-09 18:45:16 -0500

    Adding Function: New Generation and Excersice finished

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,15 +1,18 @@
 class Grid
-  attr_accessor :grid_height, :grid_width, :grid
+  attr_accessor :grid_height, :grid_width, :grid, :number_generation
 
   def initialize(data)
+    @number_generation = 1
     @grid_height = data.size
     @grid_width = data[0].size
     @grid = make_grid(data)
     check_neighbors
     print_grid
+    next_iteration
   end
 
   def print_grid
+    puts "Generation #{@number_generation}:"
     @grid_height.times do |i|
       @grid_width.times do |j|
         if @grid[i][j].alive === 0
@@ -20,6 +23,7 @@ class Grid
       end
       puts ''
     end
+    puts ' '
   end
 
   def make_grid(data)
@@ -73,16 +77,58 @@ class Grid
       end
     end
   end
+
+  def next_state
+    @grid.each do |row|
+      row.each do |cell|
+        alive = 0
+        cell.neighbors.each do |neighbor|
+          if neighbor.alive == 1
+            alive += 1
+          end
+        end
+      if cell.alive == 1
+        if alive < 2
+          cell.next_state = 0
+        elsif alive > 3
+          cell.next_state = 0
+        else
+          cell.next_state = 1
+        end
+      else
+        if alive == 3
+          cell.next_state = 1
+        end
+      end
+      end
+    end
+  end
+
+  def update_grid
+    @number_generation += 1
+    @grid.each do |row|
+      row.each do |cell|
+        cell.alive = cell.next_state
+      end
+    end
+  end
+
+  def next_iteration
+    next_state
+    update_grid
+    print_grid
+  end
 end
 
 class Cell
-  attr_accessor :alive, :pos_x, :pos_y, :neighbors
+  attr_accessor :alive, :pos_x, :pos_y, :neighbors, :next_state
 
   def initialize
     @alive = 0
     @pos_x = 0
     @pos_y = 0
     @neighbors = []
+    @next_state = 0
   end
 
   def kill_cell
